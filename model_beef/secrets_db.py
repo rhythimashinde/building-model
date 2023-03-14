8f97a24951226eff6b233056f22cb0a39f9e2920
IFU\Shinde <rhythimashinde@gmail.com>
2023-03-14 16:44:23 +0100

    updates

--- model_beef/secrets_db.py
+++
@@ -1,2 +0,0 @@
-dbstring = "host=ikgpgis2.ethz.ch port=5432 dbname=postgres user=ifu  connect_timeout=2 password='lass_mich_auf_die_gebaeude_daten_zugreifen'"
-
--- model_beef/secrets_db2.py
+++
@@ -1,2 +0,0 @@
-dbstring = "host=ifu-esd-srv-4.ethz.ch port=5433 dbname=beefdb user=beef  connect_timeout=2 password='a_very_very_secure_password'"
-
